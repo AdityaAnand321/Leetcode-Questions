9f1bb1d4ce8a683cfbd9b0d9b30890ef7311f8c7
Aditya Anand <150924538+AdityaAnand321@users.noreply.github.com>
2025-11-24 09:33:04 +0530

    Time: 119 ms (95.14%), Space: 29.9 MB (21.8%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        int arr[n+1];
+        fill(arr,arr+n+1,1);
+
+        arr[0]=0;
+        if(n>=2)
+        arr[1]=1;
+
+        for(int i=2;i*i<n;i++)
+        {
+            if(arr[i])
+            {
+                for(int j=i*i;j<n;j+=i)
+                arr[j]=0;
+            }
+        }
+
+        int count=0;
+        for(int i=2;i<n;i++)
+        if(arr[i]==1)
+        count++;
+
+        return count;
+    }
+};
